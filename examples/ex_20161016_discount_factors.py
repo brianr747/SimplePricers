d4ece85ef1c85409aa52a3affc6b7769bc5ecc75
Brian Romanchuk <brianr747@gmail.com>
2016-10-12 20:08:06 -0400

    added example

--- examples/ex_20161016_discount_factors.py
+++ examples/ex_20161016_discount_factors.py
@@ -3,16 +3,10 @@ ex_20161016_discount_factors.py
 
 Example code for discount factors.
 """
-try:
-    from simplepricers.utils import create_grid
-except ImportError:
-    import sys
-    sys.path.append('..')
-    from simplepricers.utils import create_grid
+
+from simplepricers.utils import create_grid
 
 from plot_for_examples import Quick2DPlot
 
 
 Quick2DPlot(create_grid(0., 2., 1), [1., 3., 2.])
-
-opt = input('hit return to quit')
