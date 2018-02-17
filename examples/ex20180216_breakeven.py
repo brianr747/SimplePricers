72930a964c34ff430e339573778263162cdbbec8
Brian Romanchuk <brianr747@gmail.com>
2018-02-17 17:10:29 -0500

    documentation tweak

--- examples/ex20180216_breakeven.py
+++ examples/ex20180216_breakeven.py
@@ -4,6 +4,26 @@ ex20180216_breakeven.py
 Breakeven inflation sample calculations.
 
 Highly approximate, ignoring things like seasonality and calculation lags.
+
+This example is referred to in a blog posting on BondEconomics.com, which will appear on 2018-02-21.
+It will also be referred to in the upcoming report "Inflation Breakeven Analysis" by Brian Romanchuk. That
+text explains what the calculations here are supposed to do.
+
+
+Copyright 2018 Brian Romanchuk
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
 """
 
 
